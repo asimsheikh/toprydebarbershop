39d8d69f7896b8c12d18baf1d805a6f9f43a4e28
asheikh <asim.sardar.sheikh@gmail.com>
2022-09-08 17:33:49 +1000

    Adding google image

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,11 +1,16 @@
 
+import Image from 'next/image'
+
 const Reviews = () => {
   return (
     <section class="py-8 sm:py-6" id="reviews">
         <div class="reviews p-4 flex pl-28 justify-center sm:justify-start sm:pl-36 ">
             <div class="social">
-                <img class="google-logo w-7/12" loading="lazy"
-                src="https://upload.wikimedia.org/wikipedia/commons/8/8b/Google_2015_logo_colorless_mourning_period.svg"
+                <Image class="google-logo w-7/12" loading="lazy"
+                width={159}
+                height={54}
+                src="/images/google.svg"
+                fill="lightgrey"
                 alt="Google Logo" />
                 <div>
                     <p class="text-gray-400 text-lg pl-5"> 4.7 &#9733;&#9733;&#9733;&#9733;&#9733; </p>
