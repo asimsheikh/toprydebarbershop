1cadde967b0dc8a3406c2c6496beedcb5769f103
asheikh <asim.sardar.sheikh@gmail.com>
2022-09-22 10:35:15 +1000

    Making nav items semi bold

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,13 +3,13 @@ const NavBar = () => {
                 <div class="nav-item sm:flex-1">
                     <span class="logo font-extrabold">TOPRYDE</span>&nbsp;Barbershop
                 </div>
-                <div class="nav-item hidden sm:inline">
+                <div class="nav-item hidden sm:inline font-semibold">
                     <a href="#testimonials">Testimonals</a>
                 </div>
-                <div class="nav-item hidden sm:inline">
+                <div class="nav-item hidden sm:inline font-semibold">
                     <a href="#services">Our Services</a>
                 </div>
-                <div class="nav-item hidden sm:inline">
+                <div class="nav-item hidden sm:inline font-semibold">
                     <a href="#gallery">Our Work</a>
                     </div>
         </nav>
