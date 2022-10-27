66e63e00db9db943bdd383a17cc7099be69a0b1e
asheikh <asim.sardar.sheikh@gmail.com>
2022-10-27 16:00:48 +1100

    Moving component to main index.jsx file from temp.jsx file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,37 @@
-import Temp from './temp'
+import Head from 'next/head'
+
+import NavBar from '../components/Nav'
+import Hero from '../components/Hero'
+import Reviews from '../components/Reviews'
+import Testimonials from '../components/Testimonials'
+import Services from '../components/Services'
+import Gallery from '../components/Gallery'
+import ContactLocation from '../components/ContactLocation'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
-    <Temp />
+        <Head>
+            <title>Top Ryde Barber Shop</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description"
+                content="We are a barber shop with the best fades, haircuts, styles and trims in the Sydney. located in Top Ryde Sydney." />
+            <meta name="google-site-verification" content="U-qSmQqNPntzuM8OYh5w9BhNxojFoKpPGHLA9N9p9PQ" />
+        </Head>
+
+        <body class="bg-[#000000ad]">
+        <div class="container mx-auto text-white font-['Montserrat']">
+            <NavBar />
+            <Hero />
+            <Reviews />
+            <Testimonials />
+            <Services />
+            <Gallery />
+            <ContactLocation />
+            <Footer />
+        </div>
+        </body>
     </>
   );
 };
